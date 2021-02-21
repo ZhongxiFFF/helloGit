bc7a59f9e9e2eddbf375b6248a66ca6fc9e5c5bb
FENG ZHONGXI <1226348684@qq.com>
2021-02-21 19:15:08 +0800

    123

---
+++ HelloGit/src/main/java/zhongxi/git/HelloGit.java
@@ -0,0 +1,15 @@
+package zhongxi.git;
+/*
+@projectName git_workspace
+@author Joe C Von
+@create 2021/2/21-19:06
+@description 
+*/
+
+public class HelloGit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+
+}
